72b80d43cef2afcdfb4a48cd470a5197afddb233
Kamil "MrKomugiko" Mąka <mr.komugiko@gmail.com>
2018-04-28 15:17:56 +0200

    Dodanie bazowego kodu parsera strumieniowego z uzyciem XMLREADER'a

---
+++ XML-ParserStrumieniowy/XML-ParserStrumieniowy/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace XML_ParserStrumieniowy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sumawieku = 0;
+            int liczbaelementow;
+            using (XmlReader reader = XmlReader.Create("test.xml"))
+            {
+                while (reader.Read())//wczytanie kolejnego węzła   
+                {
+                    // Sprawdzanie czy jest to element startowy     
+                    if (reader.IsStartElement())
+                    {
+                        // Sprawdzenie nazwy aktualnego węzła  
+                        switch (reader.Name)
+                        {
+                            case "wiek":
+                            Console.WriteLine("Start elementu wiek.");
+                                // Wczytanie kolejnego elementu – skoro aktualny jest elementem początkowym to następny powinien być treścią elementu.  
+                                if (reader.Read())
+                                { 
+                                    Console.WriteLine("  Zawartosc: " + reader.Value.Trim());
+                                }
+                                break;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
